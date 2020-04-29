dc929965f09599040659c91756da429f3918f981
kozyaiprog <kojiko430430@gmail.com>
2020-04-29 14:46:34 +0900

    render関数に書き換え

--- reverseneter/talking/views.py
+++ reverseneter/talking/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context={'air':''}
+    return render(request,'talking/index.html',context)
