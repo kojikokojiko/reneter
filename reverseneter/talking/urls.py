03a60a1a13cc5ba9a6dcc50ce2cb28e158cad3f8
kozyaiprog <kojiko430430@gmail.com>
2020-05-05 00:24:12 +0900

    neter、themeのパスを追加

--- reverseneter/talking/urls.py
+++ reverseneter/talking/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 app_name='talking'
 
+
+# 第一引数の後に/を追加するのを忘れないように
 urlpatterns = [
     path('', views.index, name='index'),
-    path('neter',views.neter,name='neter'),
-    path('path',views.theme,name='theme'),
+    path('neter/',views.neter,name='neter'),
+    path('theme/',views.theme,name='theme'),
 ]
