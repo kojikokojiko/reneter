a736556bbe1c36b4c28d6f1445f5feb5e283497b
kozyaiprog <kojiko430430@gmail.com>
2020-05-05 00:24:55 +0900

    pathの後ろにスラッシュをついかしてエラー解決

--- reverseneter/reverseneter/urls.py
+++ reverseneter/reverseneter/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+# 第一引数の後に/を追加するのを忘れないように
+
 urlpatterns = [
-    path('talking', include('talking.urls')),
+    path('talking/', include('talking.urls')),
     path('admin/', admin.site.urls),
     # accountsが続いていた場合に用意していたviewを呼び出す
     path('accounts/', include('django.contrib.auth.urls')),
